e13e41e9ddbbf718e40bcf81f77d26cd1826faef
ipetruk <ipetruk@circle.cloud>
2024-03-29 18:55:18 +0200

    iced test setup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
-fn main() {
-    println!("Hello, world!");
+use iced::widget::button;
+use iced::{Sandbox, Settings};
+
+fn main() -> iced::Result {
+    TestWindow::run(Settings::default())
+}
+
+struct TestWindow {}
+
+#[derive(Debug, Clone, Copy)]
+enum Message {
+    ShowPressed,
+}
+
+impl Sandbox for TestWindow {
+    type Message = Message;
+
+    fn new() -> Self {
+        Self {}
+    }
+
+    fn title(&self) -> String {
+        String::from("Test window")
+    }
+
+    fn update(&mut self, message: Self::Message) {
+        match message {
+            Message::ShowPressed => {
+                println!("TEST");
+            }
+        }
+    }
+
+    fn view(&self) -> iced::Element<'_, Self::Message> {
+        button("Button").on_press(Message::ShowPressed).into()
+    }
 }
