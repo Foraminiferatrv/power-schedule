f46f08c854135bfa2c5ca63c2f628b4cf2bd1754
Foraminiferatrv <ione.trv@gmail.com>
2024-12-22 21:58:53 +0200

    feat: First egui setup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
 // mod ui;
+use eframe::egui;
+fn main() {
+    let native_options = eframe::NativeOptions::default();
 
-fn main() {}
+    let _app = eframe::run_native(
+        "Power Schedule",
+        native_options,
+        Box::new(|cc| Ok(Box::new(PowerScheduleEguiApp::new(cc)))),
+    );
+}
+
+#[derive(Default)]
+struct PowerScheduleEguiApp {}
+
+impl PowerScheduleEguiApp {
+    fn new(cc: &eframe::CreationContext<'_>) -> Self {
+        Self::default()
+    }
+}
+
+impl eframe::App for PowerScheduleEguiApp {
+    fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.heading("Hello Power Schedule!");
+        });
+    }
+}
